d591ef5261d06a36cd862bdd50bb8a5bcaf5b6d8
scottjustason <64607427+scottjustason@users.noreply.github.com>
2022-10-03 13:18:08 -0400

    updating from coursera examples

---
+++ js/script.js
@@ -0,0 +1,15 @@
+<div class="container-fluid">
+        <div class="row">
+            <div class="col-md-3">Col1</div>
+            <div class="col-md-3">Col2</div>
+            <div class="col-md-8">Col3</div>
+            <div class="col-md-3">Col4</div>
+        </div>
+    </div>
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-md-4">Col1</div>
+            <div class="col-md-4">Col2</div>
+            <div class="col-md-6">Col3</div>
+        </div>
+    </div>
